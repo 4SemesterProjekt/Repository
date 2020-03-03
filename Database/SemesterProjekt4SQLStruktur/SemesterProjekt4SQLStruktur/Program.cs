1b4d70d5432136c639d1df44eab9b13f618c456a
Markus Kristian Simonsen <MarkMusen2635@gmail.com>
2020-03-03 21:31:29 +0100

    Added structure for database in entity framework
    Files:
    + Bruger.cs
    + Gruppe.cs
    + dbContext.cs
    + Item.cs
    + Migrations
    + Pantry.cs
    + Recipe.cs
    + ShoppingList.cs
    
    Functionality:
    Added DB-structure in Entity Framework (written in Jetbrains Rider)

---
+++ Database/SemesterProjekt4SQLStruktur/SemesterProjekt4SQLStruktur/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SemesterProjekt4SQLStruktur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new DbContext())
+            {
+                
+            }
+        }
+    }
+}
