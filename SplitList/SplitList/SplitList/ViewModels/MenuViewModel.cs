79338c66eff0168448f813f4d1b8ab667c566eed
Dahlbom <60405256+Dahlbom@users.noreply.github.com>
2020-03-10 12:59:11 +0100

    Added a property and an attribute  to MenuViewModel.cs

--- SplitList/SplitList/SplitList/ViewModels/MenuViewModel.cs
+++ SplitList/SplitList/SplitList/ViewModels/MenuViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using Prism.Mvvm;
+using SplitList.Utility;
 
 namespace SplitList.ViewModels
 {
-    class MenuViewModel
+    public class MenuViewModel : BindableBase
     {
+        public MenuViewModel()
+        {
+            _navigationItems = new ObservableCollection<NavigationItem>();
+            _navigationItems.Add(new NavigationItem());
+        }
+
+        public ObservableCollection<NavigationItem> _navigationItems;
+
+        public ObservableCollection<NavigationItem> NavigationItems
+        {
+            get => _navigationItems; 
+            set => SetProperty(ref _navigationItems, value);
+        }
     }
 }
--- SplitList/SplitList/SplitList/ViewModels/ShoppingListViewModel.cs
+++ SplitList/SplitList/SplitList/ViewModels/ShoppingListViewModel.cs
@@ -9,21 +9,22 @@ using SplitList.Models;
 namespace SplitList.ViewModels
 {
     public class ShoppingListViewModel : BindableBase
-    { 
-    public ShoppingListViewModel()
     {
-    _items = new ObservableCollection<Item>();
-    _items.Add(new Item("Banana", 4, "Fruit"));
-    _items.Add(new Item("Apple", 3, "Fruit"));
-    _items.Add(new Item("Milk", 1, "Dairy"));
-    _items.Add(new Item("Rye Bread", 1, "Bread"));
-    }
+        public ShoppingListViewModel()
+        {
+            _items = new ObservableCollection<Item>();
+            _items.Add(new Item("Banana", 4, "Fruit"));
+            _items.Add(new Item("Apple", 3, "Fruit"));
+            _items.Add(new Item("Milk", 1, "Dairy"));
+            _items.Add(new Item("Rye Bread", 1, "Bread"));
+        }
 
-    private ObservableCollection<Item> _items;
+        private ObservableCollection<Item> _items;
 
-    public ObservableCollection<Item> Items
-    {
-    get => _items;
-    set => SetProperty(ref _items, value);
+        public ObservableCollection<Item> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
     }
 }
