261ad1c7be2d6d804e79333ea008a8ff90a1f3aa
Markus Kristian Simonsen <MarkMusen2635@gmail.com>
2020-03-10 15:00:32 +0100

    Added and initiated ClientAPI
    Files:
    + ClientAPI.sln
    + Group.cs
    + Item.cs
    + Pantry.cs
    + Recipe.cs
    + ShoppingList.cs
    + user.cs
    
    ~ dbContext.cs
    ~ Program.cs

---
+++ ClientAPI/ClientAPI/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace ClientAPI
+{
+    public class Recipe
+    {
+        public int RecipeID { get; set; }
+        public string Name { get; set; }
+        
+        public ICollection<ItemRecipe> ItemRecipes { get; set; }
+    }
+}
