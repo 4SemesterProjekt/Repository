2850e8be20df5ff44e8f746d53f409860418e73b
Thomas Carstensen <tcarstensen489@gmail.com>
2020-03-30 14:28:54 +0200

    Removed unused nav command

--- SplitList/SplitList/SplitList/ViewModels/MenuViewModel.cs
+++ SplitList/SplitList/SplitList/ViewModels/MenuViewModel.cs
@@ -17,30 +17,5 @@ namespace SplitList.ViewModels
         {
         }
 
-        #region Properties
-
-
-        #endregion
-
-        #region Commands
-
-        private ICommand _navgiationCommand;
-
-        public ICommand NavigationCommand
-        {
-            get { return _navgiationCommand ?? (_navgiationCommand = new DelegateCommand(() =>
-            {
-                var navPage = new MasterDetailPage()
-                {
-                    Master = new MenuView() {Title = "MenuView"},
-                    Detail = new NavigationPage(new PantryView())
-                };
-                Application.Current.MainPage = navPage;
-                
-            })); }
-        }
-
-        #endregion
-        
     }
 }
