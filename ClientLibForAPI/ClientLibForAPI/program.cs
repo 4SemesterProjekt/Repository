368650c49506fa84e54bba73574c8d826c53f737
roman800 <magnus.roman@hotmail.com>
2020-03-30 10:47:27 +0200

    Tilføjet test af groups
    +groups class using DTO.

---
+++ ClientLibForAPI/ClientLibForAPI/program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using DatabaseEntries;
+using Newtonsoft.Json;
+
+namespace ClientLibForAPI
+{
+    class program
+    {
+       
+        private static void Main() 
+        {
+            Console.WriteLine("Hello world");
+            Groups tester = new Groups();
+            var allGroups = tester.GetGroup();
+            foreach(var i in allGroups) 
+            {
+                Console.WriteLine($"{i.GroupID}, {i.Name}");
+            }
+        }
+
+    }
+}
