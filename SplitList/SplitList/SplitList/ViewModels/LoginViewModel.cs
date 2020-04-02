6ec308cab414507ee7eff462ad417d7871d0584f
MatiasTvorup <matias@tvorup.eu>
2020-04-02 13:02:45 +0200

    Front-end login klart til integration med backend login.
    Files:
    ~MainActivity: Android registreret til at håndtere callback URLs
    ~App.xaml.cs: Appen starter på login-siden.
    +LoginView.xaml: Side med login-knap
    +LoginViewModel.cs: ViewModel, der indeholder koden for login.
    
    Der mangler at blive givet en værdi til AuthenticationUri. Mangler at accesToken bliver videresendt til WebAPI. Mangler at åbne for appen, når der er logget ind.

---
+++ SplitList/SplitList/SplitList/ViewModels/LoginViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Prism.Commands;
+using Prism.Mvvm;
+using SplitList.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SplitList.ViewModels
+{
+    class LoginViewModel
+    {
+        private Uri AuthenticationUri = new Uri(""); //URL på vores webapi, som appen skal requeste et link til en loginside fra.
+        private Uri CallBackUri = new Uri("SplitList://"); //URL til vores app, som loginsiden bruger til at åbne for appen, når login er gennemført.
+
+        private ICommand _LoginCommand;
+        public ICommand LoginCommand
+        {
+            get => _LoginCommand ?? (_LoginCommand = new DelegateCommand(LoginCommandExecute));
+        }
+
+        //Laver en http-request til vores web-api, bliver viderestillet til Google-login-siden. Efter login sendes brugeren tilbage til appen via callbackURL.
+        public void LoginCommandExecute()
+        {
+            var authResult = WebAuthenticator.AuthenticateAsync(AuthenticationUri, CallBackUri);
+
+            var accesToken = authResult?.Result;
+
+            if(accesToken != null)
+            {
+                //To Do: Send accesToken til web-api og åben for MultiShopListView.
+
+
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "Login failed. Try again.", "OK");
+            }
+
+
+        }
+    }
+
+
+
+
+}
