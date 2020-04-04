1757a7198553318fb4d8809bcc8e3d56fa1714a6
Markus Kristian Simonsen <MarkMusen2635@gmail.com>
2020-04-04 11:12:14 +0200

    Changed db to be InMemory and added DB functionality to login.
    Files:
    ~ appsettings.Json
    ~ IdentityHostingStartup.cs
    ~AuthenticationTest.csproj
       Changed the DB to be InMemory
    
    + AccountController.cs
       Functionality for account login and external login.
    
    ~WeatherForecastController.cs
      Changed it back to only be Index-page.

---
+++ SplitListWebApi/AuthenticationTest/Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public string Index()
+        {
+            return "Index Reached";
+        }
+
+        [AllowAnonymous]
+        [Route("[action]")]
+        public IActionResult GoogleLogin(int id)
+        {
+            var redirectUrl = Url.Action(nameof(GoogleResponse), "Account");
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties("Google", redirectUrl);
+            return new ChallengeResult("Google", properties);
+        }
+
+        [AllowAnonymous]
+        [Route("[action]")]
+        public async Task<IActionResult> GoogleResponse()
+        {
+            ExternalLoginInfo info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+                return BadRequest("Login-user not found");
+
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, false);
+            string[] userInfo =
+                {info.Principal.FindFirst(ClaimTypes.Name).Value, info.Principal.FindFirst(ClaimTypes.Email).Value, info.Principal.FindFirst(ClaimTypes.NameIdentifier).Value};
+            if (result.Succeeded)
+                return Ok(userInfo);
+            else
+            {
+                IdentityUser user = new IdentityUser
+                {
+                    Email = info.Principal.FindFirst(ClaimTypes.Email).Value,
+                    UserName = info.Principal.FindFirst(ClaimTypes.Email).Value,
+                    Id = info.Principal.FindFirst(ClaimTypes.NameIdentifier).Value
+                };
+
+                IdentityResult identResult = await _userManager.CreateAsync(user);
+                if (!identResult.Succeeded) return Forbid();
+                identResult = await _userManager.AddLoginAsync(user, info);
+
+                if (!identResult.Succeeded) return Forbid();
+                await _signInManager.SignInAsync(user, false);
+
+                return Ok(userInfo);
+
+            }
+        }
+
+        [AllowAnonymous]
+        [Route("[action]")]
+        public string Login()
+        {
+            return "Login Reached";
+        }
+    }
+}
--- SplitListWebApi/AuthenticationTest/Controllers/WeatherForecastController.cs
+++ SplitListWebApi/AuthenticationTest/Controllers/WeatherForecastController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,32 +13,12 @@ namespace AuthenticationTest.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    [Authorize]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public string Index()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return "Index Reached";
         }
     }
 }
