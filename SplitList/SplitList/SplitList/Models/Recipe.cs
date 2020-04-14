edd127f580e2f4e38c36c24438092f6421e65c95
Frederik Dahlbom <dahlbom16@gmail.com>
2020-04-14 14:08:25 +0200

    Added Recipe Model, viewmodel and view and made a slight change to Menuview.xaml to redirect correctly from the menu.

---
+++ SplitList/SplitList/SplitList/Models/Recipe.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Prism.Mvvm;
+
+namespace SplitList.Models
+{
+    public class Recipe : BindableBase
+    {
+        public Recipe()
+        {
+            Items = new ObservableCollection<Item>();
+            Name = "";
+            Introduction = "";
+            Instructions = "";
+        }
+
+        public Recipe(string name, string introduction, string instructions)
+        {
+            Items = new ObservableCollection<Item>();
+            Name = name;
+            Introduction = introduction;
+            Instructions = instructions;
+        }
+
+        #region Properties
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+
+
+        private string _introduction;
+        public string Introduction
+        {
+            get => _introduction;
+            set => SetProperty(ref _introduction, value);
+        }
+
+
+        private ObservableCollection<Item> _items;
+
+        public ObservableCollection<Item> Items
+        {
+            get => _items; 
+            set => SetProperty(ref _items, value);
+        }
+
+        private string _instructions;
+        public string Instructions { 
+            get => _instructions; 
+            set => SetProperty(ref _instructions, value); }
+
+
+        #endregion
+    }
+}
---
+++ SplitList/SplitList/SplitList/ViewModels/MultiRecipesViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Prism.Mvvm;
+using SplitList.Models;
+using Xamarin.Forms;
+
+namespace SplitList.ViewModels
+{
+    class MultiRecipesViewModel : BindableBase
+    {
+        public MultiRecipesViewModel(INavigation navigation, Page page)
+        {
+            _page = page;
+            Navigation = navigation;
+            Recipes = new ObservableCollection<Recipe>();
+
+            Recipes.Add(new Recipe("Spegetti bolo", "Lækkereste easy mam", "Find selv ud af det."));
+            Recipes.Add(new Recipe("Spegetti carba", "Lækkereste easy mum", "Find selv ud af det."));
+            Recipes.Add(new Recipe("Spegetti bacon", "Lækkereste easy nam", "Find selv ud af det."));
+        }
+
+        #region Properties
+
+        private Page _page;
+
+        private INavigation Navigation { get; set; }
+
+        private ObservableCollection<Recipe> _recipes;
+
+        public ObservableCollection<Recipe> Recipes
+        {
+            get => _recipes; 
+            set => SetProperty(ref _recipes, value);
+        }
+
+        private Recipe _currenRecipe;
+
+        public Recipe CurrentRecipe
+        {
+            get => _currenRecipe;
+            set => SetProperty(ref _currenRecipe, value);
+        }
+
+
+        #endregion
+    }
+}
