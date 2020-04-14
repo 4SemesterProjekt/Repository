f3766348bdb47d730fcbb61e4435f1cfe7e63476
Worly97 <frede0@hotmail.com>
2020-04-14 14:08:29 +0200

    Setup pantry view + models etc
    Set up the the required files for the pantry to work, still missing connection to database

---
+++ SplitList/SplitList/SplitList/Mapping/PantryMapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using ApiFormat;
+using SplitList.Models;
+
+namespace SplitList.Mapping
+{
+    public class PantryMapper
+    {
+        public static Pantry PantryDtoToPantry(PantryDTO dto)
+        {
+            Pantry newPantry = new Pantry();
+            newPantry.Name = dto.Name;
+            newPantry.GroupName = dto.GroupName;
+            newPantry.GroupId = dto.GroupID;
+            newPantry.PantryId = dto.ID;
+            if (dto.Items != null)
+            {
+                foreach (var dtoItem in dto.Items)
+                {
+                    newPantry.Items.Add(ItemMapper.ItemDtoToItem(dtoItem));
+                }
+            }
+
+            return newPantry;
+        }
+
+        public static PantryDTO PantryToPantryDto(Pantry pantry)
+        {
+            PantryDTO newDto = new PantryDTO();
+            newDto.Items = new List<ItemDTO>();
+            newDto.GroupID = pantry.GroupId;
+            newDto.Name = pantry.Name;
+            newDto.GroupName = pantry.GroupName;
+            newDto.ID = pantry.PantryId;
+            if (pantry.Items != null)
+            {
+                foreach (var pantryItem in pantry.Items)
+                {
+                    newDto.Items.Add(ItemMapper.ItemToItemDto(pantryItem));
+                }
+            }
+
+            return newDto;
+        }
+    }
+}
--- SplitList/SplitList/SplitList/Views/PantryView.xaml.cs
+++ SplitList/SplitList/SplitList/Views/PantryView.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ApiFormat;
+using ClientLibAPI;
+using SplitList.Mapping;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,15 @@ namespace SplitList.Views
         public PantryView()
         {
             InitializeComponent();
+            PantryViewModel.Page = this;
+            //ToDo Get GroupID from user active group
+        }
+
+        protected override async void OnDisappearing()
+        {
+            base.OnDisappearing();
+            PantryDTO dto = PantryMapper.PantryToPantryDto(PantryViewModel.Pantry);
+            var result = await SerializerPantry.PostPantry(dto);
         }
     }
 }
