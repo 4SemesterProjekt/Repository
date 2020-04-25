84edbb389b7aee48bfc33fcc817268d8e13dd677
Markus Kristian Simonsen <201808680@post.au.dk>
2020-04-25 10:55:40 +0200

    Added ShoppingListItemRepository and ShoppingListService

---
+++ SplitListWebApi/SplitListWebApi/Repositories/Implementation/ShoppingListItemRepository.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApiFormat;
+using ApiFormat.Item;
+using ApiFormat.ShadowTables;
+using ApiFormat.ShoppingList;
+using ApiFormat.User;
+using SplitListWebApi.Areas.Identity.Data;
+
+namespace SplitListWebApi.Repositories.Implementation
+{
+    public class ShoppingListItemRepository
+    {
+        private SplitListContext _context;
+
+        public ShoppingListItemRepository(SplitListContext context) => _context = context;
+
+        public void CreateShoppingListItems(ItemModel itemModel, List<ShoppingListModel> shoppingListModels)
+        {
+            foreach (var shoppingListModel in shoppingListModels)
+            {
+                _context.ShoppingListItems.Add(new ShoppingListItem()
+                {
+                    ItemModel = itemModel,
+                    ShoppingListModel = shoppingListModel,
+                    ItemModelID = itemModel.ModelId,
+                    ShoppingListModelID = shoppingListModel.ModelId
+                });
+            }
+            _context.SaveChanges();
+        }
+
+        public void CreateShoppingListItems(List<ItemModel> itemModels, ShoppingListModel shoppingListModel)
+        {
+            foreach (var itemModel in itemModels)
+            {
+                _context.ShoppingListItems.Add(new ShoppingListItem()
+                {
+                    ItemModel = itemModel,
+                    ShoppingListModel = shoppingListModel,
+                    ItemModelID = itemModel.ModelId,
+                    ShoppingListModelID = shoppingListModel.ModelId
+                });
+            }
+            _context.SaveChanges();
+        }
+
+        public void DeleteShoppingListItems(List<ShoppingListItem> shoppingListItems)
+        {
+            _context.ShoppingListItems.RemoveRange(shoppingListItems);
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingListItem> GetBy(int itemId, IEnumerable<int> shoppingListIds)
+        {
+            var shoppingListItems = new List<ShoppingListItem>();
+            foreach (var shoppingListId in shoppingListIds)
+            {
+                shoppingListItems.Add(_context.ShoppingListItems.FirstOrDefault(sli => sli.ItemModelID == itemId && sli.ShoppingListModelID == shoppingListId));
+            }
+            return shoppingListItems;
+        }
+
+        public List<ShoppingListItem> GetBy(IEnumerable<int> itemIds, int shoppingListId)
+        {
+            var shoppingListItems = new List<ShoppingListItem>();
+            foreach (var itemId in itemIds)
+            {
+                shoppingListItems.Add(_context.ShoppingListItems.FirstOrDefault(sli => sli.ItemModelID == itemId && sli.ShoppingListModelID == shoppingListId));
+            }
+
+            return shoppingListItems;
+        }
+    }
+}
---
+++ SplitListWebApi/SplitListWebApi/Services/ShoppingListService.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApiFormat;
+using ApiFormat.Group;
+using ApiFormat.Item;
+using ApiFormat.ShoppingList;
+using AutoMapper;
+using SplitListWebApi.Areas.Identity.Data;
+using SplitListWebApi.Repositories.Implementation;
+using SplitListWebApi.Services.Interfaces;
+
+namespace SplitListWebApi.Services
+{
+    public class ShoppingListService : IService<ShoppingListDTO, int>
+    {
+        private SplitListContext _context;
+        private readonly IMapper _mapper;
+        private readonly GenericRepository<ShoppingListModel> _shoppingListRepository;
+        private readonly ShoppingListItemRepository _shoppingListItemRepo;
+
+        public ShoppingListService(SplitListContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+            _shoppingListRepository = new GenericRepository<ShoppingListModel>(context);
+            _shoppingListItemRepo = new ShoppingListItemRepository(context);
+        }
+
+
+        public ShoppingListDTO GetById(int id)
+        {
+            return _mapper.Map<ShoppingListDTO>(_shoppingListRepository.GetBy(model => model.ModelId == id));
+        }
+
+        public ShoppingListDTO Create(ShoppingListDTO dto)
+        {
+            var model = _mapper.Map<ShoppingListModel>(dto);
+            var dbModel = _shoppingListRepository.GetBy(m => m.ModelId == dto.ModelId);
+
+            if (dbModel == null)
+            {
+                var createdShoppingListModel = _shoppingListRepository.Create(model);
+                var itemModels = _mapper.Map<List<ItemModel>>(dto.Items);
+                _shoppingListItemRepo.CreateShoppingListItems(itemModels, createdShoppingListModel);
+
+                dbModel = _shoppingListRepository.GetBy(model => model.ModelId == createdShoppingListModel.ModelId);
+                dbModel.ShoppingListItems = _shoppingListItemRepo.GetBy(dbModel.ModelId, itemModels.Select(im => im.ModelId));
+                return _mapper.Map<ShoppingListDTO>(dbModel);
+            }
+
+            return _mapper.Map<ShoppingListDTO>(dbModel);
+        }
+
+        public ShoppingListDTO Update(ShoppingListDTO dto)
+        {
+            var model = _mapper.Map<ShoppingListModel>(dto);
+            var dbModel = _shoppingListRepository.GetBy(model => model.ModelId == dto.ModelId);
+            if (dbModel == null)
+                return _mapper.Map<ShoppingListDTO>(_shoppingListRepository.Create(model));
+
+            _shoppingListItemRepo.DeleteShoppingListItems(dbModel.ShoppingListItems);
+            _shoppingListItemRepo.CreateShoppingListItems(_mapper.Map<List<ItemModel>>(dto.Items), dbModel);
+            return _mapper.Map<ShoppingListDTO>(_shoppingListRepository.GetBy(m => m.ModelId == dbModel.ModelId));
+        }
+
+        public void Delete(ShoppingListDTO dto)
+        {
+            var model = _mapper.Map<ShoppingListDTO>(dto);
+            var dbModel = _shoppingListRepository.GetBy(model => model.ModelId == dto.ModelId);
+            if (dbModel == null) return;
+
+            _shoppingListItemRepo.DeleteShoppingListItems(dbModel.ShoppingListItems);
+            _shoppingListRepository.Delete(dbModel);
+        }
+    }
+}
