a09f248d97420a727e072614e17692eb212eee40
Worly97 <frede0@hotmail.com>
2020-04-30 14:18:42 +0200

    Added profiles for most items
    moved a few properties around as well

---
+++ SplitList/SplitList/SplitList/AutoMapper/ShoppingListProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApiFormat.ShoppingList;
+using AutoMapper;
+using SplitList.Models;
+
+namespace SplitList.AutoMapper
+{
+    class ShoppingListProfile : Profile
+    {
+        public ShoppingListProfile()
+        {
+            CreateMap<ShoppingListDTO, ShoppingList>()
+                .PreserveReferences()
+                .ForMember(sl => sl.ShoppingListId, opt => opt.MapFrom(slDto => slDto.ModelId));
+
+            CreateMap<ShoppingList, ShoppingListDTO>()
+                .PreserveReferences()
+                .ForMember(slDto => slDto.ModelId, opt => opt.MapFrom(sl => sl.ShoppingListId));
+        }
+    }
+}
